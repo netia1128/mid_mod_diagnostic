275ca7d8303f012be83231ee6d15942bb0fc205d
Netia Ingram <netia1128@hotmail.com>
2021-04-08 16:45:15 -0600

    Add test for course has attributes

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -11,5 +11,31 @@ RSpec.describe Course do
     it 'creates an instance of a course' do
       expect(@course).to be_an_instance_of(Course)
     end
+    it 'has attributes' do
+      expect(@course.name).to eq("Calculus") 
+      expect(@course.capacity).to eq(2)
+    end
   end
 end
+
+
+# pry(main)> course = Course.new("Calculus", 2)    
+# # => #<Course:0x00007fa0a69be328...>
+# pry(main)> course.name
+# # => "Calculus"
+# pry(main)> course.capacity
+# # => 2
+# pry(main)> course.students
+# # => []
+# pry(main)> course.full?
+# # => false
+# pry(main)> student1 = Student.new({name: "Morgan", age: 21})
+# # => #<Student:0x00007fa0a80ae588...>
+# pry(main)> student2 = Student.new({name: "Jordan", age: 29})    
+# # => #<Student:0x00007fa0a814f4d8...>
+# pry(main)> course.enroll(student1)    
+# pry(main)> course.enroll(student2)    
+# pry(main)> course.students
+# # => [#<Student:0x00007fa0a80ae588...>, #<Student:0x00007fa0a814f4d8...>]
+# pry(main)> course.full?
+# # => true
